a4e80f88455477c1c79607f4afdc0da04f62b158
GameTheory <gametheory@ubuntu.(none)>
2014-08-04 00:02:21 -0400

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Sun Aug 3 00:04:08 EDT 2014"
+#define LINUX_COMPILE_BY "gametheory"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
